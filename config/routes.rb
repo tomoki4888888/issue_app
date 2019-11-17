d72bb216839e5088e596638a58bc15f9cc70d58f
tomoki4888888 <tomoki.koro@gmail.com>
2019-11-17 11:24:36 +0000

    shine

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
     
+  resources :titles
   root 'static_pages#home'
   
   get  'static_pages/home'
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20191117112353) do
+
+  create_table "titles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
