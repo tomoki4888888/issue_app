6e954721843149cda51d3e34913db1e0dfd62e3e
tomoki4888888 <tomoki.koro@gmail.com>
2019-11-17 11:28:16 +0000

    kami

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
     
+  resources :comments
   resources :titles
   root 'static_pages#home'
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191117112353) do
+ActiveRecord::Schema.define(version: 20191117112746) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "come"
+    t.string "Tname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "titles", force: :cascade do |t|
     t.string "name"
